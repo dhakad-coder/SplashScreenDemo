65625731e5d955727cf2331b49a2cf734dc305d6
Ghanshyam Nayma <ghanshyam.nayma@elektrobit.com>
2022-06-02 12:00:09 +0530

    Initial commit

---
+++ app/src/main/java/com/dc/splashscreendemo/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.dc.splashscreendemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //TODO start here with your own logic
+    }
+}
